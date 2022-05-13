0d32043e27a737b4fb22178c7ce1f09741ac92f8
cristianocsl <cslcristiano@gmail.com>
2022-05-13 13:05:41 -0300

    feat: teste de renderização em vue

---
+++ src/components/TableValue.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>Olá!</h1>
+  <h1>Meu nome é {{ nome }}</h1>
+  <ul v-for="item in array_de_nomes" v-bind:key="item">
+    <li>{{ item }}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'TableValue',
+  data() {
+    return {
+      nome: 'Cristiano',
+      array_de_nomes: ['Cristiano', 'José', 'Maria', 'João'],
+    };
+  },
+};
+</script>
